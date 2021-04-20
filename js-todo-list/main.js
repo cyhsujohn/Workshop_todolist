44509ea06be3f2252a352edcda55dea59540fcbb
birdjjj <cyhsu.john@gmail.com>
2021-04-20 11:42:17 +0800

    feature done

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -1,1 +1,8 @@
-// TO DO
+document.addEventListener('DOMContentLoaded', function(){
+  document.querySelector('ul').addEventListener('click', (e) => {
+    console.log(e.target)
+    console.log(e.currentTarget)
+    document.querySelector('.checked').classList.remove('checked')
+    e.target.classList.add('checked')
+  })
+})
