c60cfd8ea160bcc78748be6badb2a81dd2468fb1
birdjjj <cyhsu.john@gmail.com>
2021-04-20 12:58:15 +0800

    feature done

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -1,1 +1,7 @@
-// TO DO
+document.addEventListener('DOMContentLoaded', function(){
+  document.querySelectorAll('.close').forEach(target => target.addEventListener('click', (e) => {
+    // console.log(e.target)
+    e.target.parentNode.remove()
+  })
+  )
+})
