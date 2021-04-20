8e1030fedafd0e9bb1817ce04ea95eb31a2c9412
birdjjj <cyhsu.john@gmail.com>
2021-04-20 14:09:24 +0800

    feature_add_delete_done

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -1,4 +1,10 @@
 document.addEventListener('DOMContentLoaded', function(){
+  document.querySelector('ul').addEventListener('click', (e) => {
+    console.log(e.target)
+    console.log(e.currentTarget)
+    e.target.classList.toggle('checked')
+  })
+
   document.querySelectorAll('.close').forEach(target => target.addEventListener('click', (e) => {
     // console.log(e.target)
     e.target.parentNode.remove()
